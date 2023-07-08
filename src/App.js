9f0ac547f16aa0e4c6020d4e44dd8b49e31f88e4
alperen-erol <alpereneroltr@gmail.com>
2023-07-08 03:09:47 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
+import Paths from "./pages/Paths";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import People from "./pages/People";
 import NotFound from "./pages/NotFound";
 import { Routes, Route } from "react-router-dom";
+import PeopleDetail from "./pages/PeopleDetail";
 
 function App() {
   return (
@@ -13,7 +15,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/people" element={<People />} />
+        <Route path="/people/" element={<PeopleDetail />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/paths" element={<Paths />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
